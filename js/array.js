b95a85340612b83f5e4356d5fda25a0464a0853a
lolu1023 <soowkw@naver.com>
2022-03-02 20:37:49 +0900

    array,object

---
+++ js/array.js
@@ -0,0 +1,13 @@
+
+const daysOfWeek = ["mon", 'tue',' wed',' thu', 'fri',' sat']
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to  the array
+daysOfWeek.push('sun');
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab")
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const a = 10;
 const b = 2;
-const myName = "luya"
+let myName = "luya"
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello" + ' ' + myName)
+console.log("hello" + ' ' + myName);
+
+myName = 'luyas';
+console.log("your new name is " + myName);
