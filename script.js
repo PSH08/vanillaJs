804515bf4db583ee3c6c255e52d84548a8788b33
lolu1023 <soowkw@naver.com>
2022-03-06 20:20:50 +0900

    이벤트리스너

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-const title = document.querySelector(".hello h1")
+const h1 = document.querySelector("div.hello:first-child h1")
 
-title.innerHTML = "Hello!"
+function handleTitleClick() {
+  h1.style.color = 'blue'
+}
+
+function handleTitleEnter() {
+  h1.innerText = "Mouse is here!"
+}
+
+function handleTitleLeave() {
+  h1.innerText = "Mouse is gone!"
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = 'tomato'
+}
+
+function handleWindowCopy() {
+  alert("copier!")
+}
+
+function handleWindowOffline() {
+  alert("Offline");
+}
+
+function handleWindowOnline() {
+  alert("Online!!")
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleTitleEnter);
+h1.addEventListener("mouseleave", handleTitleLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
