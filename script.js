68c0c78f2b639f47a3e32b6b32303a9c3091f70a
lolu1023 <soowkw@naver.com>
2022-03-09 21:46:42 +0900

    form(2)

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const loginInout = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector('#greeting');
 
-function onLoginBtnClick() {
-  const username = loginInout.ariaValueMax;
-  console.log(username)
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value
+  localStorage.setItem(USERNAME_KEY, username)
+  paintGreetings(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick)
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.add("submit", onLoginSubmit)
+} else {
+  paintGreetings(saveUsername);
+}
